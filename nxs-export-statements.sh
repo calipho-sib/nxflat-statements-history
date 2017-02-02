b725230d3fd32888cec9e8dbdb359baa069de16f
npdb npdb <npdb@crick.isb-sib.ch>
2017-02-02 13:19:19 +0100

    adding modification from latest release

--- nxs-export-statements.sh
+++ nxs-export-statements.sh
@@ -1,7 +1,7 @@
 SQL_EXPORT="
-select entry_accession, gene_name, annotation_category, location_begin_master, location_end_master, annotation_name, evidence_quality, evidence_intensity, variant_original_amino_acid, variant_variation_amino_acid, reference_accession 
+select entry_accession, gene_name, annotation_category, location_begin_master, location_end_master, annotation_name, evidence_quality, evidence_intensity, variant_original_amino_acid, variant_variation_amino_acid, reference_accession, subject_annotation_ids, object_annotation_ids, annotation_id
 from nxflat.entry_mapped_statements
-order by entry_accession, gene_name, annotation_category, location_begin_master, location_end_master, annotation_name, evidence_quality, evidence_intensity, variant_original_amino_acid, variant_variation_amino_acid, reference_accession
+order by entry_accession, gene_name, annotation_category, location_begin_master, location_end_master, annotation_name, evidence_quality, evidence_intensity, variant_original_amino_acid, variant_variation_amino_acid, reference_accession, subject_annotation_ids, object_annotation_ids, annotation_id
 "
 if [ -z "$1" ]
 then 
