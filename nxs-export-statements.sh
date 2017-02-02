9f7cca60c783c15b7cfaadd1d4ec0a81fedf1dfa
npdb npdb <npdb@crick.isb-sib.ch>
2017-02-02 11:35:53 +0100

    adding august version

--- nxs-export-statements.sh
+++ nxs-export-statements.sh
@@ -1,7 +1,11 @@
-su npdb
 SQL_EXPORT="
 select entry_accession, gene_name, annotation_category, location_begin_master, location_end_master, annotation_name, evidence_quality, evidence_intensity, variant_original_amino_acid, variant_variation_amino_acid, reference_accession 
 from nxflat.entry_mapped_statements
 order by entry_accession, gene_name, annotation_category, location_begin_master, location_end_master, annotation_name, evidence_quality, evidence_intensity, variant_original_amino_acid, variant_variation_amino_acid, reference_accession
 "
-psql -d nxflat -c "COPY ($SQL_EXPORT) TO STDOUT WITH CSV HEADER"
+if [ -z "$1" ]
+then 
+	psql -d nxflat -c "COPY ($SQL_EXPORT) TO STDOUT WITH CSV HEADER"
+else	
+	psql -h $1 -d nxflat -c "COPY ($SQL_EXPORT) TO STDOUT WITH CSV HEADER"
+fi
